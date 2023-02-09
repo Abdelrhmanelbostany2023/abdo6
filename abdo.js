3d53c9fcb4c02d449ca5eaa47243fed531c5d7f8
Abdelrhmanelbostany2023 <123656859+Abdelrhmanelbostany2023@users.noreply.github.com>
2023-02-09 11:55:31 -0800

    Add files via upload

---
+++ abdo.js
@@ -0,0 +1,23 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
+
+
+const timer = document.querySelector('#time span')
+
+setInterval(() =>{
+    timer.innertext = new Date().tolocaleStrind()
+}, 1000)
